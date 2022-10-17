b247b5cbea9f442b5c26c3bdf5b9c3d2fc8e00ca
elinkass <elinkass@tktk.ee>
2022-10-17 21:04:36 +0300

    add week6 code

---
+++ RandomElement/Program.cs
@@ -0,0 +1,14 @@
+﻿//string[] snacks = { "sushi", "pizza", "burger", "asian", "chicken" };
+
+//Random rnd = new Random();
+
+//int RandomIndex = rnd.Next(0, snacks.Length);
+
+//Console.WriteLine($"Tonight we are going to eat {snacks[RandomIndex]}.");
+
+string[] colors = { "red", "green", "blue", "pink", "yellow" };
+Random rnd = new Random();
+
+int randomColor = colors[rnd.Next(0, colors)];
+
+Console.WriteLine($"You should wear {colors} today.");
